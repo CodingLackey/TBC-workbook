24a5be6cb6860d26819adb2c93e99a181e1c2977
CodingLackey <gearare1@hknu.ac.kr>
2026-03-09 20:04:45 +0900

    Add : 03-09

--- Chapter16/16_memcpy/main.c
+++ Chapter16/16_memcpy/main.c
@@ -18,16 +18,24 @@ int main()
 	- overlapping region
 	- pointer-to-void (datatype is unknown)
 	*/
+	int arr1[LEN] = { 1, 3, 5, 7, 9, 11 };
+	int* arr2 = (int*)malloc(LEN * sizeof(int));
+	if (arr2 == NULL)
+		exit(1);
 
+	for (int i = 0; i < LEN; ++i)
+		arr2[i] = arr1[i];
 
-
+	memcpy(arr2, arr1, sizeof(int) * LEN);
+	prt(arr2, LEN);
 
 	/*
 	{ 1, 3, 5, 7, 9, 11 }
 	{ 5, 7, 9, 11, 9, 11}
 	*/
- 
 
-
+	memcpy(arr1, &arr1[2], sizeof(int) * 4); //undefined behavior
+	//memmove(arr1, &arr1[2], sizeof(int) * 4);
+	prt(arr1, LEN);
 	return 0;
 }
