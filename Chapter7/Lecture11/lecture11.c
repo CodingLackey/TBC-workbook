186942006e9321383a742367cd8c2ed1270ecb53
CodingLackey <gearare1@hknu.ac.kr>
2026-03-18 22:21:04 +0900

    Add : 03-18

--- Chapter7/Lecture11/lecture11.c
+++ Chapter7/Lecture11/lecture11.c
@@ -1,10 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-//#include <float.h>
+#include <float.h>
 
 int main()
 {
+	float max = -FLT_MAX;
+	float min = FLT_MAX;
+	float sum = 0.0f;
+	float input;
+	int n = 0;
 
+	while (scanf("%f", &input) == 1)
+	{
+		if((input > 0) && (input < 101))
+		{
+			max = (input > max) ? input : max;
+			min = (input > min) ? min : input;
+
+			sum += input;
+		}
+	}
+	printf("max is %f, min is %f sum is %f", max, min, sum);
 
 	return 0;
 }
