ff0eff7e2c06d90a30c004401d6045a6397c09b4
CodingLackey <gearare1@hknu.ac.kr>
2026-03-19 11:23:36 +0900

    Add : 03-19

--- Chapter8/Lecture9/lecture9.c
+++ Chapter8/Lecture9/lecture9.c
@@ -1,12 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-//#include <stdlib.h>	// exit()
+#include <stdlib.h>	// exit()
 
 int main()
 {
+	int c;
+	FILE* file = NULL;
+	char file_name[] = "my_file.txt";
 
+	file = fopen(file_name, "r"); //fopen의 반환값이 file 변수에 저장
+	
+	if (file == NULL)
+	{
+		printf("Failed to open file.\n");
+		exit(1); // Code 1004
+	}
+	while ((c = getc(file)) != EOF)
+		putchar(c);
 
-
+	fclose(file);
 
 	return 0;
 }
